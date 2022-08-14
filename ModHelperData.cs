0567f42c6ed4c6b4144b7001224df1f51607f35e
Manker5678 <101741242+Manker5678@users.noreply.github.com>
2022-08-14 17:18:13 -0500

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -1,11 +1,13 @@
-namespace ParagonLostWeapons;
-
-public static class ModHelperData
+namespace ParagonLostWeapons
 {
-    public const string Name = "Paragon Lost Weapons";
-    public const string Description = "Restores lost weapons, such as caltrops and the bloon trap, to the Paragon";
-    public const string Version = "1.0.0";
-    public const string RepoOwner = "Manker5678";
-    public const string RepoName = "ParagonLostWeapons";
-    public const string Author = "DeeEks";
+    public static class ModHelperData
+    {
+        public const string Name = "Paragon Lost Weapons";
+        public const string Description = "Restores lost weapons, such as caltrops and the bloon trap, to the Paragon";
+        public const string Version = "1.0.0";
+        public const string RepoOwner = "Manker5678";
+        public const string RepoName = "ParagonLostWeapons";
+        public const string Author = "DeeEks";
+    }
+
 }
