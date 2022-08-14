470fd5604f3d8c3cba7b2a8225d7c2cc7920ae09
Manker5678 <101741242+Manker5678@users.noreply.github.com>
2022-08-14 14:31:02 -0500

    Add files via upload

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -8,7 +8,8 @@ namespace ParagonLostWeapons
         public const string Description = "Restores weapons lost by some tower's Paragons, such as Caltrops/Cleansing Foam/Bloon Trap";
 
         public const string RepoOwner = "Manker5678"; // TODO add your github username hero, also in the download url in README.md
-        public const string RepoName = ""; // TODO add your repo name here, also in the download url in README.md
+        public const string RepoName = "ParagonLostWeapons"; // TODO add your repo name here, also in the download url in README.md
+        public const string Author = "DeeEks";
     }
 }
 
